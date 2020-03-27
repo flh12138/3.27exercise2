b74043773c2ec7c4dd1ccd2a3bd1d2e21e0c4cee
FLH0202 <FLH0202@163.com>
2020-03-27 14:22:21 +0800

    l

---
+++ exercise2/main.cpp
@@ -0,0 +1,33 @@
+#include<opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	VideoCapture cap;
+
+	cap.open(0);
+
+	double fps = cap.get(CAP_PROP_FPS);
+	std::cout << "fps" << fps << std::endl;
+
+
+	while (1)
+	{
+		cv::Mat frame;
+		cv::Mat BlurMat;
+		cap.read(frame);
+		blur(frame, BlurMat, Size(16,16),Point(-1,-1));
+		imshow("BlurMat", BlurMat);
+		imshow("frame", frame);
+		waitKey(30);
+
+	}
+
+
+
+
+
+
+}
